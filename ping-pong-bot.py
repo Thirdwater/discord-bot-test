b07b7418305c85f1d919fcdcc4091ad8925ecd0d
Thirdwater <irinthirdwater@gmail.com>
2020-10-15 10:22:16 +0100

    add simple bot

---
+++ ping-pong-bot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == 'ping':
+        await message.channel.send('pong')
+
+client.run(TOKEN)
